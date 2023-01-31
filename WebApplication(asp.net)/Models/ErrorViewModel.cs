61b77cfa701327a64425188ef74fd40ab2996ae8
venka <venka@SUDHA>
2023-01-31 18:48:32 +0530

    Add project files.

---
+++ WebApplication(asp.net)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication_asp.net_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
